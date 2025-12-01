ce9d62edccc5e3c77e42b9a40b7475c489b4f0c1
youesffkatama <youesffkatama@gmail.com>
2025-12-01 18:52:05 +0200

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var os = require('os');
 const { connectDB, User, Log, Notification, EmailLog, Note, Journal, BookStats } = require('./models');
 
 var PORT = process.env.PORT || 3000;
-var API_KEY = process.env.OPENROUTER_API_KEY || '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';
+var API_KEY = process.env.OPENROUTER_API_KEY || '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';
 //MONGODB_URI="mongodb+srv://yusufkaram:Church@bibleai.pewfze2.mongodb.net/bibleai?appName=bibleai" OPENROUTER_API_KEY="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" npm start
 
 if (!API_KEY) {
@@ -964,3 +964,4 @@ process.on('SIGINT', function() {
   });
 
 });
+
